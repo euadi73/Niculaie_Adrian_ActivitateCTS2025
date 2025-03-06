171f9d3fd0869eecbea1c4b9470bde6914249bf1
euadi73 <niculaieadrian20@stud.ase.ro>
2025-03-06 23:20:13 +0200

    sem2commit2

--- src/ro/cts/clase/Angajat.java
+++ src/ro/cts/clase/Angajat.java
@@ -5,41 +5,49 @@ import java.util.Arrays;
 public class Angajat extends Aplicant {
 	private String ocupatie;
 	private int salariu;
-	private static int sumaFinantata;
-	
-	public String getOcupatie() {
-		return ocupatie;
-	}
-	public void setOcupatie(String ocupatie) {
-		this.ocupatie = ocupatie;
-	}
-	public int getSalariu() {
-		return salariu;
-	}
-	public void setSalariu(int salariu) {
-		this.salariu = salariu;
-	}
+	private static final int SUMA_FINANTATA = 100; // Valoare exemplu, se poate modifica
 
-	public Angajat(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumire_Proiecte,int salariu,String ocupatie) {
-		super(nume,prenume,varsta,punctaj,nr_proiecte,denumire_Proiecte);
+	public Angajat(String nume, String prenume, int varsta, int punctaj, int nrProiecte, String[] denumireProiecte, int salariu, String ocupatie) {
+		super(nume, prenume, varsta, punctaj, nrProiecte, denumireProiecte);
 		this.salariu = salariu;
 		this.ocupatie = ocupatie;
 	}
 
 	public Angajat() {
-		super();
-		
+		this("", "", 0, 0, 0, new String[]{}, 0, "");
+	}
+
+	public String getOcupatie() {
+		return ocupatie;
+	}
+
+	public void setOcupatie(String ocupatie) {
+		this.ocupatie = ocupatie;
+	}
+
+	public int getSalariu() {
+		return salariu;
+	}
+
+	public void setSalariu(int salariu) {
+		this.salariu = salariu;
+	}
+
+	public static int getSumaFinantata() {
+		return SUMA_FINANTATA;
 	}
 
 	@Override
 	public String toString() {
-		return "Angajat: Nume=" + nume + ", Prenume=" + prenume
-				+ ", Varsta=" + varsta + ", Punctaj=" + punctaj + ", Nr_proiecte=" + nr_proiecte + ", DenumireProiect="
-				+ Arrays.toString(denumireProiect) + "Ocupatie=" + ocupatie + ", salariu=" + salariu;
+		return new StringBuilder()
+				.append("Angajat: ")
+				.append(super.toString())
+				.append(", Ocupatie=").append(ocupatie)
+				.append(", Salariu=").append(salariu)
+				.toString();
 	}
 
 	public void afiseazaInformatiiFinantare() {
-		System.out.println("Angajatul "+getNume()+" "+getPrenume()+" primeste"+ sumaFinantata +" Euro/zi in proiect.");
+		System.out.println("Angajatul " + getNume() + " " + getPrenume() + " primeste " + SUMA_FINANTATA + " Euro/zi in proiect.");
 	}
-	
 }
