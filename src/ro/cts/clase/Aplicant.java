2824358f155160e2a3dc925ec027cb1e042ca54c
euadi73 <niculaieadrian20@stud.ase.ro>
2025-03-06 23:24:33 +0200

    AplicantRefactor

--- src/ro/cts/clase/Aplicant.java
+++ src/ro/cts/clase/Aplicant.java
@@ -1,65 +1,71 @@
 package ro.cts.clase;
 
-public abstract class Aplicant{
+public abstract class Aplicant {
 	protected String nume;
 	protected String prenume;
 	protected int varsta;
 	protected int punctaj;
-	protected int nr_proiecte;
-	protected String[] denumireProiect;
-	
+	protected int nrProiecte;
+	protected String[] denumireProiecte;
+
+	public Aplicant() {
+		this("", "", 0, 0, 0, new String[]{});
+	}
+
+	public Aplicant(String nume, String prenume, int varsta, int punctaj, int nrProiecte, String[] denumireProiecte) {
+		this.nume = nume;
+		this.prenume = prenume;
+		this.varsta = varsta;
+		this.punctaj = punctaj;
+		this.nrProiecte = nrProiecte;
+		this.denumireProiecte = denumireProiecte;
+	}
+
 	public String getNume() {
 		return nume;
 	}
+
 	public void setNume(String nume) {
 		this.nume = nume;
 	}
+
 	public String getPrenume() {
 		return prenume;
 	}
+
 	public void setPrenume(String prenume) {
 		this.prenume = prenume;
 	}
+
 	public int getVarsta() {
 		return varsta;
 	}
+
 	public void setVarsta(int varsta) {
 		this.varsta = varsta;
 	}
 
-	public void statut(){
-		if(punctaj>80)
-			System.out.println("Aplicantul "+nume+" "+prenume+" a fost acceptat.");
-		else
-			System.out.println("Aplicantul "+nume+" "+prenume+" nu a fost acceptat.");
-		}
-
 	public int getPunctaj() {
 		return punctaj;
 	}
+
 	public void setPunctaj(int punctaj) {
 		this.punctaj = punctaj;
 	}
-	
-	public Aplicant() {
-		super();
-	}
-	public Aplicant(String nume, String prenume, int varsta, int punctaj, int nr_proiecte, String[] denumireProiect) {
-		super();
-		this.nume = nume;
-		this.prenume = prenume;
-		this.varsta = varsta;
-		this.punctaj = punctaj;
-		this.nr_proiecte = nr_proiecte;
-		this.denumireProiect = denumireProiect;
-	}
-	public int getNr_proiecte() {
-		return nr_proiecte;
-	}
-	public void setNr_proiecte(int nr_proiecte, String[] denumireProiecte) {
-		this.nr_proiecte = nr_proiecte;
-		this.denumireProiect = denumireProiecte;
-	}
-	public abstract void afiseazaInformatiiFinantare();
 
+	public int getNrProiecte() {
+		return nrProiecte;
 	}
+
+	public void setNrProiecte(int nrProiecte, String[] denumireProiecte) {
+		this.nrProiecte = nrProiecte;
+		this.denumireProiecte = denumireProiecte;
+	}
+
+	public void afiseazaStatut() {
+		String mesaj = (punctaj > 80) ? "a fost acceptat." : "nu a fost acceptat.";
+		System.out.println("Aplicantul " + nume + " " + prenume + " " + mesaj);
+	}
+
+	public abstract void afiseazaInformatiiFinantare();
+}
